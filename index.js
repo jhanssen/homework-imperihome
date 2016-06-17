ffaaf4296e7278707dc97f8696448d6612c174d3
Jan Erik Hanssen <jhanssen@gmail.com>
2016-06-17 14:35:57 -0700

    Imperihome support

---
+++ index.js
@@ -0,0 +1,190 @@
+/*global require, module, setTimeout*/
+
+"use strict";
+
+const http = require("http");
+const util = require('util');
+const url = require('url');
+const EventEmitter = require('events');
+const crypto = require('crypto');
+
+function convertType(type)
+{
+    switch (type) {
+    case "Dimmer":
+        return "DevDimmer";
+    case "Light":
+    case "Fan":
+        return "DevSwitch";
+    case "Door":
+        return "DevDoor";
+    case "Sensor":
+        return "DevMotion";
+    case "RGBWLed":
+        return "DevRGBLight";
+    case "Thermostat":
+        return "DevThermostat";
+    }
+    return undefined;
+}
+
+const roomToID = Object.create(null);
+const IDtoRoom = Object.create(null);
+
+function roomId(room, floor)
+{
+    if (room == undefined)
+        room = "";
+    if (floor == undefined)
+        floor = "";
+    let r = room + ":" + floor;
+    if (r in roomToID)
+        return roomToID[r];
+
+    const hash = crypto.createHash('sha1').update(r).digest('hex');
+    roomToID[r] = hash;
+    IDtoRoom[hash] = { room: room, floor: floor };
+    return hash;
+}
+
+function findDevice(homework, uuid)
+{
+    var devs = homework.devices;
+    if (!(devs instanceof Array))
+        return undefined;
+    return devs.find((d) => { return d.uuid == uuid; });
+}
+
+function handleRequest(req, res)
+{
+    const parsedUrl = url.parse(req.url);
+    const path = parsedUrl.pathname.split("/").filter((p) => { return p.length > 0; });
+
+    const handlers = {
+        devices: (path, write) => {
+            if (!path.length) {
+                // list all devices
+                let devices = this.homework.devices;
+                let ret = [];
+                for (var i = 0; i < devices.length; ++i) {
+                    var itype = convertType(hdev.type);
+                    if (itype !== undefined) {
+                        var hdev = devices[i];
+                        var idev = {
+                            id: hdev.uuid,
+                            name: hdev.name,
+                            room: roomId(hdev.room, hdev.floor),
+                            type: itype
+                        };
+                        var hvals = hdev.values;
+                        var ivals = [];
+                        for (var hvk in hvals) {
+                            var hval = hvals[hvk];
+                            ivals.push({
+                                key: hvk,
+                                value: hval.value,
+                                unit: hval.unit,
+                                graphable: false
+                            });
+                        }
+                        idev.params = ivals;
+                        ret.push(idev);
+                    }
+                }
+                write(ret);
+            } else if (path.length == 4 && path[1] == "action") {
+                // set value
+                let device = findDevice(this.homework, path[0]);
+                if (device) {
+                    if (path[2] in device.values) {
+                        console.log(`setting device to ${path[3]}`);
+                        device.values[path[2]].value = path[3];
+
+                        write({ success: true });
+                    } else {
+                        write("Couldn't find value");
+                    }
+                } else {
+                    write("Couldn't find device");
+                }
+            } else {
+                write("Invalid path");
+            }
+        },
+        rooms: (path, write) => {
+            let rooms = {};
+            let devices = this.homework.devices;
+            for (var i = 0; i < devices.length; ++i) {
+                var device = devices[i];
+                var floor = device.floor || "";
+                if (device.room && device.room.length) {
+                    if (floor.length)
+                        floor += " ";
+                    floor += device.room;
+                }
+                if (floor.length)
+                    rooms[floor] = { room: device.room, floor: device.floor };
+            }
+            let ret = [];
+            for (var room in rooms) {
+                ret.push({ id: roomId(rooms[room].room, rooms[room].floor),
+                           name: room });
+            }
+            write(ret);
+        },
+        system: (path, write) => {
+            write({ id: "homework", apiversion: 0 });
+        }
+    };
+
+    if (path[0] in handlers) {
+        handlers[path[0]](path.slice(1), (obj) => {
+            if (typeof obj === "object") {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(obj));
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                if (typeof obj === "string")
+                    res.end(obj);
+                else
+                    res.end("404 Bad request");
+            }
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("404 Path not found");
+    }
+}
+
+const imperihome = {
+    _homework: undefined,
+    _server: undefined,
+
+    get name() { return "imperihome"; },
+    get ready() { return true; },
+
+    get homework() { return this._homework; },
+
+    init: function(cfg, data, homework) {
+        if (!cfg || !cfg.device || !(cfg.claps instanceof Array) || !cfg.claps.length) {
+            return false;
+        }
+
+        this._homework = homework;
+
+        var port = cfg.port || 8095;
+
+        this._server = http.createServer(handleRequest.bind(this));
+        this._server.listen(port);
+
+        EventEmitter.call(this);
+        return true;
+    },
+    shutdown: function(cb) {
+        cb();
+    }
+};
+
+util.inherits(imperihome, EventEmitter);
+
+module.exports = imperihome;
